c171436b542fc5303d1f1f7263642827f8a72287
Drayton-Dobbs <42974676+Drayton-Dobbs@users.noreply.github.com>
2024-03-12 12:00:57 -0500

    feat: messing around

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <main className="bg-black"></main>;
+  return (
+    <main className="bg-green-400 h-screen flex justify-center items-center">
+      <h1 className="text-3xl font-bold text-white underline">
+        Test Quick Deploy
+      </h1>
+    </main>
+  );
 }
