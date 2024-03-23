7987974d9ad48bd416794be4dd973645e8f3ab34
Drayton-Dobbs <42974676+Drayton-Dobbs@users.noreply.github.com>
2024-03-23 15:32:35 -0500

    remove comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="w-full bg-main pt-10 border-b-2 border-b-highLight">
       <div className="flex ">
         <div className="flex flex-col">
           <h2 className="text-5xl font-bold text-white mb-6">
-            Hey there, I'm <span className=" text-[#5005f2]">Drayton</span>.
+            Hey there, I&apos;m <span className=" text-[#5005f2]">Drayton</span>
+            .
           </h2>
           <p className="text-gray-400">
             Welcome to my space where I blend my journey as a full-stack
